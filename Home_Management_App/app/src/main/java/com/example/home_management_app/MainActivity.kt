465d187ea591155a6154a900c23313a985bbdc68
TaejungKim <tae0803@naver.com>
2023-09-28 13:55:47 +0900

    firebase 연결
    1. compiler.xml / .idea\gradle.xml / misc/xml 은 가급적 보내시는거 지양합니다.
    2. firebase 연동 확인했습니다. 각자 pull 해서 연동되는지 확인 부탁드립니다.

--- Home_Management_App/app/src/main/java/com/example/home_management_app/MainActivity.kt
+++ Home_Management_App/app/src/main/java/com/example/home_management_app/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.home_management_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.Sampler.Value
+import com.example.home_management_app.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    lateinit var FDB : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //데이터베이스 초기화
+        FDB = FirebaseDatabase.getInstance().reference
+
+        binding.btnAdd.setOnClickListener {
+            initAdd()
+        }
+
+        binding.btnDelete.setOnClickListener {
+            initDelete()
+        }
+
+        setContentView(binding.root)
     }
+
+    private fun initAdd(){
+        FDB.child("New").setValue("First")
+        FDB.child("Old").setValue("First2")
+    }
+
+    private fun initDelete(){
+
+    }
+
+
 }
