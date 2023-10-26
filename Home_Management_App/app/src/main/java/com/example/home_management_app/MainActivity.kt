f88978ce30096cfc21c5082fdeb4e33557c8f1e6
TaejungKim <tae0803@naver.com>
2023-10-26 23:16:10 +0900

    기본 bottom bar navigation
    초기 Bottom Bar 추가

--- Home_Management_App/app/src/main/java/com/example/home_management_app/MainActivity.kt
+++ Home_Management_App/app/src/main/java/com/example/home_management_app/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.home_management_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.home_management_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.navigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_calendar -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_frm, CalendarFragment())
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.menu_calculator -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, CalculatorFragment()).commit()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.menu_my_page -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MyPageFragment()).commit()
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> {
+                    return@setOnItemSelectedListener true
+                }
+            }
+        }
+
+        binding.navigation.selectedItemId = R.id.menu_calendar
     }
 }
